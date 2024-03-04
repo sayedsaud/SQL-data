0f9df35c6d5daca6d05be1a1fc69156eeba8bf78
sayedsaud <saudsayed08@gmail.com>
2024-03-04 18:55:13 +0530

    'create-table'

---
+++ create table.sql
@@ -0,0 +1,7 @@
+create database itvedant;
+show databases;
+use itvedant;
+create table student(
+stud_id int,stud_name varchar(300),stud_age int,stud_city varchar(300));
+show tables;
+desc student; 
